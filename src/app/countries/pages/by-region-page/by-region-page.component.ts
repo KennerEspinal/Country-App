222bdd2ab02e80efaffa6e1a76d8bfb16b86bcee
KennerEspinal <kennerespinalg@gmail.com>
2024-03-22 14:49:37 -0500

    Regions

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountryService } from '../../services/country.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,10 +12,14 @@ import { CountryService } from '../../services/country.service';
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
   constructor(private countryService: CountryService) { }
 
-  searchByRegion(region: string): void {
+  searchByRegion(region: Region): void {
+    this.selectedRegion = region;
+
     this.countryService.searchRegion(region)
       .subscribe(countries => {
         this.countries = countries;
