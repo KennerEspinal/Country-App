8eaf66acb08ce70fbd8f7917800e7cec44d4e5a4
KennerEspinal <kennerespinalg@gmail.com>
2024-03-22 04:05:38 -0500

    Servicios y Tablas

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'countries-by-capital-page',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class ByCapitalPageComponent {
+  public countries: Country[] = [];
+
+  constructor(private countryService: CountryService) { }
+
+  searchByCapital(capital: string): void {
+    this.countryService.searchCapital(capital)
+      .subscribe(countries => {
+        this.countries = countries;
+      })
+  }
+
 
 }
